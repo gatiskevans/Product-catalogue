0dc84a04d7f63b9b3d081f31ef7c8accb0bf6bb0
Gatis Ķevans <gatis.kevans@gmail.com>
2021-10-24 14:51:50 +0300

    Added Messages.php For Storing Constats of Messages

---
+++ app/Validation/Messages.php
@@ -0,0 +1,18 @@
+<?php
+
+const TITLE_EXISTS = "Product already exists";
+const TITLE_REQUIRED = "Title is required";
+const CATEGORY_REQUIRED = "Category is required";
+const QUANTITY_REQUIRED = "Quantity is required";
+const QUANTITY_NUMERIC = "Quantity must be a number";
+const INVALID_CATEGORY = "Invalid category";
+const INVALID_SORT = "Invalid Sort Option";
+const INVALID_ORDER = "Order Option Is Invalid";
+
+const LOGIN_SUCCESS = "Login Successful!";
+const REGISTRATION_SUCCESS = "Registration Successful!";
+const USER_UPDATE_SUCCESS = "Information Updated Successfully!";
+const USER_DELETE_SUCCESS = "User Deleted Successfully!";
+
+const PRODUCT_ADD_SUCCESS = "Product Added Successfully!";
+const PRODUCT_UPDATE_SUCCESS = "Edited Successfully!";
